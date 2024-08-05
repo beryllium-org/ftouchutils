3d3c527c8995738c9264fc294ad48ecb6e724596
Bill Sideris <bill88t@feline.gr>
2024-08-06 00:47:12 +0300

    Fix rmftouch

--- files/rmftouch.py
+++ files/rmftouch.py
@@ -5,7 +5,7 @@ if not ("h" in vr("opts")["o"] or "help" in vr("opts")["o"]):
     if len(vr("opts")["w"]):
         vr("devt", vr("opts")["w"][0])
         if vr("devt").startswith("/dev/ftouch"):
-            be.based.run("rmnod " + vr("devt"))
+            be.based.run("rmnod " + vr("devt")[5:])
             be.api.setvar("return", "0")
         else:
             term.write("Error: Invalid device!")
